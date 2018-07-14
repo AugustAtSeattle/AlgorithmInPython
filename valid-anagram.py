5872f6b910c8f1f48bc63ace6e152a4fc7ab10ca
AugustAtSeattle <august.lin.usa@gmail.com>
2018-07-14 07:23:18 -0700

    Add Alternative Solution

--- valid-anagram.py
+++ valid-anagram.py
@@ -12,6 +12,19 @@ class Solution:
             dic2[c] = dic2.get(c,0) + 1
         return dic1 == dic2
 
+    def isAnagram2(self, s, t):
+        list1, list2 = [0]*26, [0]*26
+        for c in s:
+            list1[ord(c)-ord('a')] +=1
+        for c in t:
+            list2[ord(c)-ord('a')] +=1
+        return list1 == list2
+
+    def isAnagram3(self, s, t):
+        return sorted(s) == sorted(t)
+
 if __name__ == "__main__" :
     s = Solution()
     print(s.isAnagram("ABB","BBA"))
+    print(s.isAnagram2("abb","bba"))
+    print(s.isAnagram3("bba","abb"))
