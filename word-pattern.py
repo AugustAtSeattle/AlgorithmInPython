09a35ce2520d2030df12f298778baacfefa9f2d1
August <august.lin.usa@gmail.com>
2018-07-16 20:35:48 -0700

    add new solution

--- word-pattern.py
+++ word-pattern.py
@@ -14,6 +14,11 @@ class Solution:
             else:
                 dic[c] = s
 
+    def wordPattern2(self, pattern, str):
+        s = pattern
+        t = str.split()
+        return map(s.find, s) == map(t.index, t)
+
 if __name__ == "__main__" :
     s = Solution()
     print(s.wordPattern("abba", "dog cat cat dog"))
